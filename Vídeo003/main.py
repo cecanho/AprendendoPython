db7f25902a4b5ee0a9002ad9204935cf2213dcc0
Cristiano José Cecanho <cecanho@gmail.com>
2022-08-23 06:24:17 -0300

    Tipos embutidos, variáveis e palavras reservadas
    Conceito de tipos embutidos, variáveis e palavras reservadas

---
+++ Vídeo003/main.py
@@ -0,0 +1,37 @@
+# Autor: Cristiano José Cecanho
+# Data: 19-08-2022
+# Fonte: Apostila Caelum
+# Título: Conceito de tipos embutidos, variáveis e palavras reservadas
+
+# Função de inpressão
+print('Vídeo 003')
+
+# Tipos embutidos: int, float e str (inteiro, real e literal)
+num_inteiro = 10
+num_real = 3.14
+str_nome = '10'
+
+# Impressão de um inteiro
+print(num_inteiro)
+
+# Alteração do valor atribuído à variável
+num_inteiro = 8
+
+# Impressão do valor alterado e a diferença entre inteiro e literal
+print(num_inteiro)
+print(str_nome)
+
+# Classificação e impressão dos tipos embutidos
+print(type(num_inteiro))
+print(type(num_real))
+print(type(str_nome))
+
+# Palavras reservadas
+# - int, float, str, i, f
+# - and del from None True
+# - as elif global nonlocal try
+# - assert else if not while
+# - break except import or with
+# - class False in pass yield
+# - continue finaly is raise def
+# - for lambda return
