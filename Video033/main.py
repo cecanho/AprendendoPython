47c20e1a571b57adb7f3ae231729219ac0900c70
Cristiano José Cecanho <cecanho@gmail.com>
2023-01-15 07:51:44 -0300

    Atualizações do vídeo 034

--- Video033/main.py
+++ Video033/main.py
@@ -1,13 +1,16 @@
 # Autor: Cristiano José Cecanho
-# Aprendendo Python: Vídeo 033
+# Aprendendo Python: Vídeo 033-034
 # Data: 14-01-2023
 # Objetivo: OO em Python - Métodos
 
 from Conta import Conta
 
-cnt1 = Conta('Cristiano José Cecanho', '001-1', 100.0, 1000.0)
+cnt1 = Conta('Neymar Jr', '001-1', 100.0, 1000.0)
+print(cnt1.extrato())
+print()
+cnt2 = Conta('Cristiano José Cecanho', '002-1', 100.0, 1000.0)
 # print(cnt1.saldo)
-
+'''
 print(cnt1.extrato())
 
 cnt1.setLimite(1500.0)
@@ -21,3 +24,10 @@ print()
 print(cnt1.extrato())
 
 cnt1.saca(2000.0)
+'''
+
+cnt1.transfere(cnt2, 100.0)
+
+print(cnt1.extrato())
+print()
+print(cnt2.extrato())
