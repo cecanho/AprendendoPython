08faf8593e6376e72d4fbb13bc48b78543e83fc3
Cristiano José Cecanho <cecanho@gmail.com>
2023-01-30 20:44:13 -0300

    Alteração do video 037

--- Video033/Conta.py
+++ Video033/Conta.py
@@ -2,6 +2,9 @@
 # Aprendendo Python: Vídeo 033-034-035
 # Data: 21-01-2023
 # Objetivo: OO em Python
+import datetime
+
+from Historico import Historico
 
 class Conta:
 # Construtor da classe
@@ -10,13 +13,16 @@ class Conta:
         self.numero = numero
         self.saldo = saldo
         self.limite = limite
+        self.historico = Historico()
 
     def deposita(self, valor):
         self.saldo += valor
+        self.historico.transacoes.append(f"Depósito de {valor}")
 
     def saca(self, valor):
         if (self.limite + self.saldo >= valor):
             self.saldo -= valor
+            self.historico.transacoes.append(F"Saque de {valor}")
         else:
             print(f"Saldo insuficiente para saque/transferência: {self.limite + self.saldo}\n")
             return False
@@ -27,6 +33,7 @@ class Conta:
                 f"Saldo (a): {self.saldo}\n" \
                 f"Limite (b): {self.limite}\n" \
                 f"Saldo Total (a+b): {self.limite + self.saldo}"
+        self.historico.transacoes.append(f"Tirou extrato dia {datetime.datetime.today()}")
         return frase
 
     def setTitular(self, titular):
@@ -41,5 +48,5 @@ class Conta:
             return False
         else:
             destino.deposita(valor)
+            self.historico.transacoes.append(f"Transferência de {valor} para a conta {destino.numero}")
             return True
-
