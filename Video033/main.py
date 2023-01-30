c72c07c4bf2a9e1e04ee1542472b6de00cb76909
Cristiano José Cecanho <cecanho@gmail.com>
2023-01-30 20:44:51 -0300

    Alteração do video 037

--- Video033/main.py
+++ Video033/main.py
@@ -1,55 +1,36 @@
 # Autor: Cristiano José Cecanho
-# Aprendendo Python: Vídeo 033-034-035-036
-# Data: 21-01-2023
-# Objetivo: OO em Python - Agregação
+# Aprendendo Python: Vídeo 033-034-035-036-037
+# Data: 29-01-2023
+# Objetivo: OO em Python - Agregação/Composição
 
 from Conta import Conta
 from ClientePF import ClientePF
 from Endereco import Endereco
 
-ender1 = Endereco('n. 51',
-                    '',
+endereco = Endereco('n. 1',
+                    'Fundos',
                     'Avenida Ulisses Guimarães',
-                    'Vila Indaiá',
+                    'Vila indaiá',
                     'Rio Claro',
                     'SP',
                     'Brasil',
                     '13500-000')
 
-clienteNeymar = ClientePF('Neymar',
-                          'Jr',
-                          '123.456.789-10',
-                          '12.345.678-8',
-                          ender1,
-                          '19-1234-6789',
-                          '19-98765-4321',
-                          '19-9876-5432')
+cliente1 = ClientePF('João', 'Oliveira', '111111111-11', '12345678-9', endereco, '3333-3333', '99999-9999', '3333-3333')
+cliente2 = ClientePF('José', 'Azevedo', '222222222-22', '12345679-9', endereco, '3333-3333', '99999-9998', '3333-3333')
 
-cnt1 = Conta(clienteNeymar, '001-1', 1000000.0)
-# print(f"{str.upper(cnt1.titular.nome)}\nSALDO: {cnt1.saldo}")
-print(cnt1.extrato())
+conta1 = Conta(cliente1, '123-4', 1000.0)
+conta2 = Conta(cliente2, '123-5', 1000.0)
 
+conta1.deposita(100.0)
+conta1.saca(50.0)
+conta1.transfere(conta2, 200.0)
+
+print(type(conta1.titular))
+print(conta1.extrato())
+
+
+conta1.historico.imprime()
 print()
-print(type(cnt1))
-
-print()
-print(type(clienteNeymar))
-
-print()
-print(type(ender1))
-print(cnt1.titular.endereco.logradouro)
-
-
-'''
-cnt1 = Conta('Neymar Jr', '001-1', 100.0, 1000.0)
-print(cnt1.extrato())
-print()
-cnt2 = Conta('Cristiano José Cecanho', '002-1', 100.0)
-print(cnt2.extrato())
-print()
-
-cnt1.transfere(cnt2, 1200.0)
-print(cnt1.extrato())
-print()
-print(cnt2.extrato())
-print()'''
+print(conta2.extrato())
+conta2.historico.imprime()
