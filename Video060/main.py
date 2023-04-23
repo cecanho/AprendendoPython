ee7752a3d9ca4d77fde887dd86c0da87716e4b6e
Cristiano José Cecanho <cecanho@gmail.com>
2023-04-23 07:30:35 -0300

    Exceções e tipos de erros

---
+++ Video060/main.py
@@ -0,0 +1,23 @@
+# Autor: Cristiano José Cecanho
+# Aprendendo Python: Vídeo 060
+# Data: 19-04-2023
+# Objetivo: Exceções e Tipos de Erros
+
+# NameError
+# print(x)
+
+# TypeError
+# lista = [1, 2, 3]
+# print(lista['a'])
+
+# KeyError
+# dicionario = {'nome': 'João', 'idade': 25}
+# print(dicionario['cidade'])
+
+# AttributeError
+# lista = [1, 2, 3]
+# print(lista.nome)
+
+# IndexError
+tupla = (1, 2, 3)
+print(tupla[3])
